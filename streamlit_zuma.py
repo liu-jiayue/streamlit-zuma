ef21f3f1023559bb195c0c42e216871aa10aed8a
JIAYUE LIU <liu.jiayue@orange.fr>
2022-05-22 20:43:12 +0200

    Created a new app

---
+++ streamlit_zuma.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import streamlit as st
+from google.cloud import firestore
+
+# Authenticate to Firestore with the JSON account key.
+db = firestore.Client.from_service_account_json("firestore-key.json")
+
+# Create a reference to the Google post.
+doc_ref = db.collection("posts").document("KoDmVv2IIEOiMBIoINP0")
+
+# Then get the data at that reference.
+doc = doc_ref.get()
+
+# Let's see what we got!
+st.write("The id is: ", doc.id)
+st.write("The contents are: ", doc.to_dict())(
